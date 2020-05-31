3ac86e1f2f5caae3395cf76d88ccd93ab7db7c16
Adrian Pruszkowski <pruszkowski.adrian@gmail.com>
2020-05-31 08:41:56 +0100

    Add files via upload

--- where-am-I/main.js
+++ where-am-I/main.js
@@ -1,6 +1,16 @@
 const h1 = document.querySelector('h1');
 
 document.body.addEventListener('mousemove', (e) => {
+
+  const x = e.clientX;
+  const y = e.clientY;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
   h1.textContent = `${e.clientX}, ${e.clientY}`;
-  console.log(e.clientX, e.clientY);
+
+  const red = x / width * 100;;
+  const green = y / height * 100;
+  const blue = 100;
+  document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
 });
